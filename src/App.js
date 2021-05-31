6085f99ca0137e471e181ed060298c870951edf0
Luisfer <luisfmanzanoa@gmail.com>
2021-05-31 10:44:26 -0400

    Tasks ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Task1 from './Task1';
+import Task2 from './Task2';
+import Task3 from './Task3';
+import Task4 from './Task4';
+import Bonus from './Bonus';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+        <nav className="navbar">
+        <Link to="/task1"><a href="#">Task 1</a></Link>
+        <Link to="/task2"><a href="#">Task 2</a></Link>
+        <Link to="/task3"><a href="#">Task 3</a></Link>
+        <Link to="/task4"><a href="#">Task 4</a></Link>
+        <Link to="/bonus"><a href="#">BONUS TASK</a></Link>
+        </nav>
+
+        <div className="separator"></div>
+
+        <Switch>
+            <Route path="/task1">
+              <Task1 />
+            </Route>
+            <Route path="/task2">
+              <Task2 />
+            </Route>
+            <Route path="/task3">
+              <Task3 />
+            </Route>
+            <Route path="/task4">
+              <Task4 />
+            </Route>
+            <Route path="/bonus">
+              <Bonus />
+            </Route>
+          </Switch>
+
+        </Router>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+class Navbar extends React.Component {
+
+    
+    render() {
+        return(
+            <h1>h</h1>
+        )
+    }
+}
+
+export default Navbar;
